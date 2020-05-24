9870df0603c432706748aa9e548e11a288865a09
Xatpy <jaime.chapinal@gmail.com>
2020-05-24 13:50:51 +0200

    Adding search box functionality

--- app2.js
+++ app2.js
@@ -6,21 +6,19 @@ const INDEX_URL_IMAGE = 4;
 
 // Removing the list of games where we don't have time information
 function cleanIncompleteData(data) {
-    debugger
     let gamesWithInfo = data;
     for (let i = 0; i < data.length; ++i) {
         if (Number(data[INDEX_MAIN]) > 0 || Number(data[INDEX_MAIN_EXTRA]) > 0 || Number(data[INDEX_COMPLETIONIST]) > 0 ) {
             gamesWithInfo.push(data[i]);
         } else {
-            console.log("Game without info: " + data[0]);
+            console.log("Game without info: " + data[i]);
         }
     }
     return gamesWithInfo;
 }
 
 function createD3Table() {
-    var column_names = ["name","gameplayMain","gameplayMainExtra","gameplayCompletionist","imageUrl"];
-    //var clicks = [title: 0, gameplayMain: 0, creatgameplayMainExtra: 0, gameplayCompletionist: 0};
+    var column_names = ["Name","Gameplay Main","Gameplay Main Extra","Gameplay Completionist","Cover"];
     var clicks = [0,0,0,0];
 
     // draw the table   
@@ -60,6 +58,7 @@ function createD3Table() {
     );
 
     var rows, row_entries, row_entries_no_anchor, row_entries_with_anchor;
+
     d3.text("./data/list.csv", function(response) {
         var data = d3.csv.parseRows(response);
         data.shift(); // Remove first row
@@ -80,178 +79,204 @@ function createD3Table() {
             .append("tr")
 
         // enter td's in each row
-    row_entries = rows.selectAll("td")
-    .data(function(d, i) { 
-        var arr = [];
-        for (var k in d) {
-            if (d.hasOwnProperty(k)) {
-                arr.push(d[k]);
+        row_entries = rows.selectAll("td")
+        .data(function(d, i) { 
+            var arr = [];
+            for (var k in d) {
+                if (d.hasOwnProperty(k)) {
+                    arr.push(d[k]);
+                }
             }
-        }
-        return [arr[INDEX_NAME],arr[INDEX_MAIN],arr[INDEX_MAIN_EXTRA],arr[INDEX_COMPLETIONIST],arr[INDEX_URL_IMAGE]];
-    })
-    .enter()
-    .append("td") 
-    .each(function(d, i) {
-        if (i > 0 && i % 4 === 0) {
-                var imgData = [];
-                imgData.push(d);
-                d3.select(this).selectAll("img").data(imgData)
-                .enter()
-                .append("img") // doesn't append an <img> anywhere
-                .attr("src", d)
-                .attr("height", "40px")
-                .attr("width", "30px");
-        }
-    })
+            return [arr[INDEX_NAME],arr[INDEX_MAIN],arr[INDEX_MAIN_EXTRA],arr[INDEX_COMPLETIONIST],arr[INDEX_URL_IMAGE]];
+        })
+        .enter()
+        .append("td") 
+        .each(function(d, i) {
+            if (i > 0 && i % 4 === 0) {
+                    var imgData = [];
+                    imgData.push(d);
+                    d3.select(this).selectAll("img").data(imgData)
+                    .enter()
+                    .append("img") // doesn't append an <img> anywhere
+                    .attr("src", d)
+                    .attr("height", "40px")
+                    .attr("width", "30px");
+            }
+        })
 
-// draw row entries with no anchor 
-    row_entries_no_anchor = row_entries.filter(function(d) {
-        return (/https?:\/\//.test(d) == false)
-    })
-    row_entries_no_anchor.text(function(d) { return d; })
+    // draw row entries with no anchor 
+        row_entries_no_anchor = row_entries.filter(function(d) {
+            return (/https?:\/\//.test(d) == false)
+        })
+        row_entries_no_anchor.text(function(d) { return d; })
 
-// draw row entries with anchor
-    row_entries_with_anchor = row_entries.filter(function(d) {
-        return (/https?:\/\//.test(d) == true)  
-    })
-    row_entries_with_anchor
-        .append("a")
-        .attr("href", function(d) { return d; })
+    // draw row entries with anchor
+        row_entries_with_anchor = row_entries.filter(function(d) {
+            return (/https?:\/\//.test(d) == true)  
+        })
+        row_entries_with_anchor
+            .append("a")
+            .attr("href", function(d) { return d; })
             .attr("target", "_blank")
             .text(function(d) { 
                 //return d; 
                 return null;
             })
-        });
 
-    /**  sort functionality **/
-  headers
-  .on("click", function(d) {
-    let sortIndex = -1;
-    switch (d) {
-        case "name":
-            sortIndex = 0;
-        break;
-        case "gameplayMain":
-            sortIndex = INDEX_MAIN;
-        break;
-        case "gameplayMainExtra":
-            sortIndex = INDEX_MAIN_EXTRA;
-        break;
-        case "gameplayCompletionist":
-            sortIndex = INDEX_COMPLETIONIST;
-        break;
-        case "imageUrl":
-            sortIndex = 4;
-        break;
-    }
-    if (sortIndex > -1) {
-        clicks[sortIndex] += 1;
-    }
-      
-    if (sortIndex === 0) {
-      // even number of clicks
-      if (clicks[sortIndex] % 2 == 0) {
-        // sort ascending: alphabetically
-        rows.sort(function(a,b) { 
-          if (a[INDEX_NAME].toUpperCase() < b[INDEX_NAME].toUpperCase()) { 
-            return -1; 
-          } else if (a[INDEX_NAME].toUpperCase() > b[INDEX_NAME].toUpperCase()) { 
-            return 1; 
-          } else {
-            return 0;
-          }
-        });
-      // odd number of clicks  
-      } else if (clicks[sortIndex] % 2 != 0) { 
-        // sort descending: alphabetically
-        rows.sort(function(a,b) { 
-          if (a[INDEX_NAME].toUpperCase() < b[INDEX_NAME].toUpperCase()) { 
-            return 1; 
-          } else if (a[INDEX_NAME].toUpperCase() > b[INDEX_NAME].toUpperCase()) { 
-            return -1; 
-          } else {
-            return 0;
-          }
-        });
-      }
-    } else if (sortIndex > 0 && sortIndex < 5) {
-        if (clicks[sortIndex] % 2 == 0) {
-            // sort ascending: numerically
-            rows.sort(function(a,b) { 
-              if (Number(a[sortIndex]) < Number(b[sortIndex])) { 
-                return -1; 
-              } else if (Number(a[sortIndex]) > Number(b[sortIndex])) { 
-                return 1; 
-              } else {
-                return 0;
-              }
-            });
-          // odd number of clicks  
-          } else if (clicks[sortIndex] % 2 != 0) { 
-            // sort descending: numerically
-            rows.sort(function(a,b) { 
-              if (Number(a[sortIndex]) < Number(b[sortIndex])) { 
-                return 1; 
-              } else if (Number(a[sortIndex]) > Number(b[sortIndex])) { 
-                return -1; 
-              } else {
-                return 0;
-              }
-            });
-          }
-    }
-})
-    
-}
 
-    /*var parsedCSV = d3.csv.parseRows(response);
-    debugger
-    d3.json("data.json", function(data) { // loading data from server
-  
-        // draw table body with rows
-        table.append("tbody")
-      
-        // data bind
-        rows = table.select("tbody").selectAll("tr")
-          .data(data, function(d){ return d.id; });
-        
-        // enter the rows
-        rows.enter()
-          .append("tr")
-        
-        // enter td's in each row
-        row_entries = rows.selectAll("td")
-            .data(function(d) { 
-              var arr = [];
-              for (var k in d) {
-                if (d.hasOwnProperty(k)) {
-                  arr.push(d[k]);
+        /**  sort functionality **/
+        headers
+        .on("click", function(d) {
+            let sortIndex = -1;
+            switch (d) {
+                case column_names[INDEX_NAME]:
+                    sortIndex = INDEX_NAME;
+                break;
+                case column_names[INDEX_MAIN]:
+                    sortIndex = INDEX_MAIN;
+                break;
+                case column_names[INDEX_MAIN_EXTRA]:
+                    sortIndex = INDEX_MAIN_EXTRA;
+                break;
+                case column_names[INDEX_COMPLETIONIST]:
+                    sortIndex = INDEX_COMPLETIONIST;
+                break;
+                case column_names[INDEX_URL_IMAGE]:
+                    sortIndex = INDEX_URL_IMAGE;
+                break;
+            }
+            if (sortIndex > -1) {
+                clicks[sortIndex] += 1;
+            }
+            
+        if (sortIndex === INDEX_NAME) {
+        // even number of clicks
+            if (clicks[sortIndex] % 2 == 0) {
+            // sort ascending: alphabetically
+                rows.sort(function(a,b) { 
+                    if (a[INDEX_NAME].toUpperCase() < b[INDEX_NAME].toUpperCase()) { 
+                        return -1; 
+                    } else if (a[INDEX_NAME].toUpperCase() > b[INDEX_NAME].toUpperCase()) { 
+                        return 1; 
+                    } else {
+                        return 0;
+                    }
+                });
+            // odd number of clicks  
+            } else if (clicks[sortIndex] % 2 != 0) { 
+            // sort descending: alphabetically
+                rows.sort(function(a,b) { 
+                if (a[INDEX_NAME].toUpperCase() < b[INDEX_NAME].toUpperCase()) { 
+                    return 1; 
+                } else if (a[INDEX_NAME].toUpperCase() > b[INDEX_NAME].toUpperCase()) { 
+                    return -1; 
+                } else {
+                    return 0;
                 }
-              }
-              return [arr[3],arr[1],arr[2],arr[0]];
-            })
-          .enter()
-            .append("td") 
-      
-        // draw row entries with no anchor 
-        row_entries_no_anchor = row_entries.filter(function(d) {
-          return (/https?:\/\//.test(d) == false)
+            });
+        }
+        } else if (sortIndex > INDEX_NAME && sortIndex < INDEX_URL_IMAGE) {
+            if (clicks[sortIndex] % 2 == 0) {
+                // sort ascending: numerically
+                rows.sort(function(a,b) { 
+                if (Number(a[sortIndex]) < Number(b[sortIndex])) { 
+                    return -1; 
+                } else if (Number(a[sortIndex]) > Number(b[sortIndex])) { 
+                    return 1; 
+                } else {
+                    return 0;
+                }
+                });
+            // odd number of clicks  
+            } else if (clicks[sortIndex] % 2 != 0) { 
+                // sort descending: numerically
+                rows.sort(function(a,b) { 
+                if (Number(a[sortIndex]) < Number(b[sortIndex])) { 
+                    return 1; 
+                } else if (Number(a[sortIndex]) > Number(b[sortIndex])) { 
+                    return -1; 
+                } else {
+                    return 0;
+                }
+                });
+            }
+        }
         })
-        row_entries_no_anchor.text(function(d) { return d; })
-      
-        // draw row entries with anchor
-        row_entries_with_anchor = row_entries.filter(function(d) {
-          return (/https?:\/\//.test(d) == true)  
-        })
-        row_entries_with_anchor
-          .append("a")
-          .attr("href", function(d) { return d; })
-          .attr("target", "_blank")
-        .text(function(d) { return d; })
-    }*/
 
+        /**  search functionality **/
+        d3.select("#search")
+        .on("keyup", function() { // filter according to key pressed 
+            var searched_data = data,
+                text = this.value.trim();
+            
+            var searchResults = searched_data.map(function(r) {
+                var regex = new RegExp("^" + text + ".*", "i");
+                if (regex.test(r[INDEX_NAME])) { // if there are any results
+                    return regex.exec(r[INDEX_NAME])[0]; // return them to searchResults
+                } 
+            })
+            
+            // filter blank entries from searchResults
+            searchResults = searchResults.filter(function(r){ 
+            return r != undefined;
+            })
+            
+            // filter dataset with searchResults
+            searched_data = searchResults.map(function(r) {
+                return data.filter(function(p) {
+                    return p[INDEX_NAME].indexOf(r) != -1;
+                })
+            })
+
+            // flatten array 
+            searched_data = [].concat.apply([], searched_data)
+            
+            // data bind with new data
+            rows = table.select("tbody").selectAll("tr")
+            .data(searched_data, function(d){ 
+                return d;
+            })
+            
+            // enter the rows
+            rows.enter()
+            .append("tr");
+            
+            // enter td's in each row
+            row_entries = rows.selectAll("td")
+                .data(function(d) { 
+                    var arr = [];
+                    for (var k in d) {
+                        if (d.hasOwnProperty(k)) {
+                            arr.push(d[k]);
+                        }
+                    }
+                    return [arr[INDEX_NAME],arr[INDEX_MAIN],arr[INDEX_MAIN_EXTRA],arr[INDEX_COMPLETIONIST],arr[INDEX_URL_IMAGE]];
+                })
+                .enter()
+                .append("td") 
+
+            // draw row entries with no anchor 
+            row_entries_no_anchor = row_entries.filter(function(d) {
+                return (/https?:\/\//.test(d) == false)
+            })
+            row_entries_no_anchor.text(function(d) { return d; })
+
+            // draw row entries with anchor
+            row_entries_with_anchor = row_entries.filter(function(d) {
+            return (/https?:\/\//.test(d) == true)  
+            })
+            row_entries_with_anchor
+            .append("a")
+            .attr("href", function(d) { return d; })
+            .attr("target", "_blank")
+            .text(function(d) { return d; })
+            
+            // exit
+            rows.exit().remove();
+        })
+
+    });
+}
 
 window.onload = function() {
     createD3Table();
